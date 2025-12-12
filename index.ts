1c040d274fbcc80966d8677545406395cdfc5f60
Martins100-Tmd <olaitanfabayo@gmail.com>
2025-12-12 17:14:54 +0100

    push: 3

--- index.ts
+++ index.ts
@@ -9,40 +9,76 @@ import { authMiddleware } from "./middleware/auth";
 import { helpRequestRouter } from "./routes/helpRouter";
 import { documentRouter } from "./routes/documentRouter";
 import { sessionRouter } from "./routes/sessionRouter";
+import { Server } from "socket.io";
+
 config({ path: "./.env" });
 
 const app = express();
 const httpServer = createServer(app);
-app.use(express.json({ limit: "500mb" }));
-app.set("port", 3000);
 
+app.use(express.json({ limit: "500mb" }));
+
+// CORS
 const whiteList = ["http://localhost:3000", "https://martins100-tmd.github.io"];
 const corsOption: CorsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || whiteList.indexOf(origin) != -1) {
+    origin(origin, callback) {
+        if (!origin || whiteList.includes(origin)) {
             callback(null, true);
-            return;
         } else {
             callback(new Error("CORS blocked origin " + origin));
         }
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}
-app.use(cors(corsOption))
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOption));
 
-
+// Routers
 app.use("/auth", authRouter);
 app.use("/help", authMiddleware, helpRequestRouter);
 app.use("/doc", authMiddleware, documentRouter);
 app.use("/session", authMiddleware, sessionRouter);
-app.get("/", async (req: Request, res: Response) => {
-    res.status(200).json({ message: "Server is running!" })
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Server is running with Socket.io!" });
 });
 
-const Port = process.env.PORT;
+// SOCKET.IO
+const io = new Server(httpServer, {
+    cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+    console.log("connected:", socket.id);
+
+    socket.on("join-room", (roomId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit("peer-joined", { socketId: socket.id });
+    });
+
+    socket.on("signal", ({ roomId, to, data }) => {
+        if (to) io.to(to).emit("signal", { from: socket.id, data });
+        else socket.to(roomId).emit("signal", { from: socket.id, data });
+    });
+
+    socket.on("draw", ({ roomId, data }) => {
+        socket.to(roomId).emit("draw", data);
+    });
+
+    socket.on("clear-board", (roomId) => {
+        socket.to(roomId).emit("clear-board");
+    });
+
+    socket.on("disconnect", () => {
+        console.log("disconnected:", socket.id);
+    });
+});
+
+// SERVER START
+const PORT = process.env.PORT || 3000;
+
 await connectDB().then(() => {
-    httpServer.listen(Port, () => {
-        console.log("Server is running");
-    })
-})
+    httpServer.listen(PORT, () => {
+        console.log("Server + Socket.io running on", PORT);
+    });
+});
